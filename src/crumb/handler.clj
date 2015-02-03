f5166cefc06432cc011b02a35ab89d5600c4aaf3
Chris McCluskey <christopher.i.mccluskey@gmail.com>
2015-02-03 18:53:02 +0000

    Added basic romaji/hiragana test

--- src/crumb/handler.clj
+++ src/crumb/handler.clj
@@ -2,11 +2,19 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [crumb.create-layout :as create]))
+            [crumb.create-layout :as create]
+            [crumb.test-layout :as test]
+            [namban.boeki :as namban]))
+
+(defn get-test-word
+  []
+  (namban/romaji->hiragana "watashi"))
 
 (defroutes app-routes
   (GET "/" [] (create/form))
   (POST "/" {{word :word} :params} (create/translation word))
+  (GET "/test" [] (test/test-submission-page (get-test-word)))
+  (POST "/test" {{answer :answer} :params} (test/test-result-page answer "watashi"))
   (route/not-found "Not Found"))
 
 (def app
